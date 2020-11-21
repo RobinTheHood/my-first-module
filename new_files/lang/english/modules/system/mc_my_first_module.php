e24762c69f405fd6a45af85efc47eff7ef88172d
Robin Wieschendorf <mail@robinwieschendorf.de>
2020-11-21 18:37:25 +0100

    initial commit

---
+++ new_files/lang/english/modules/system/mc_my_first_module.php
@@ -0,0 +1,5 @@
+<?php
+define('MODULE_MC_MY_FIRST_MODULE_TITLE', 'mycompany/my-first-module © by <a href="#" target="_blank" style="color: #e67e22; font-weight: bold;">mycompany</a>');
+define('MODULE_MC_MY_FIRST_MODULE_LONG_DESCRIPTION', 'Long description for mycompany/my-first-module');
+define('MODULE_MC_MY_FIRST_MODULE_STATUS_TITLE', 'mycompany/my-first-module Modul active?');
+define('MODULE_MC_MY_FIRST_MODULE_STATUS_DESC', '');
---
+++ new_files/lang/german/modules/system/mc_my_first_module.php
@@ -0,0 +1,5 @@
+<?php
+define('MODULE_MC_MY_FIRST_MODULE_TITLE', 'mycompany/my-first-module © by <a href="#" target="_blank" style="color: #e67e22; font-weight: bold;">mycompany</a>');
+define('MODULE_MC_MY_FIRST_MODULE_LONG_DESCRIPTION', 'Lange Beschreibung für mycompany/my-first-module');
+define('MODULE_MC_MY_FIRST_MODULE_STATUS_TITLE', 'mycompany/my-first-module Modul aktivieren?');
+define('MODULE_MC_MY_FIRST_MODULE_STATUS_DESC', '');
